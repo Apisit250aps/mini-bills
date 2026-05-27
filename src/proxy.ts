ee5118531899dfde783b5cc3450b21c9e7894cd6
U <88958116+Apisit250aps@users.noreply.github.com>
2026-05-27 20:39:19 +0700

    feat: update scrollbar styles in globals.css and enhance proxy authentication logic

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,25 @@
-import { auth } from "@/auth"
- 
+import { auth } from '@/auth'
+import { NextResponse } from 'next/server'
+
 export const proxy = auth((req) => {
-  // req.auth
+  const pathname = req.nextUrl.pathname
+  const auth = req.auth
+
+  if (['/me', '/'].includes(pathname)) {
+    if (!auth) {
+      return NextResponse.redirect(new URL('/login', req.nextUrl))
+    }
+  }
+  if (pathname === '/login') {
+    if (auth) {
+      return NextResponse.redirect(new URL('/', req.nextUrl))
+    }
+  }
+
+  return NextResponse.next()
 })
- 
+
 // Optionally, don't invoke Proxy on some paths
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
